b8f2fda0490a206541a624883b622bfdfe623f20
Olga-Smolianinova <olga.smol2015@gmail.com>
2020-10-06 11:34:28 +0300

    master

---
+++ js/codewars.js
@@ -0,0 +1,36 @@
+// function multiply(a, b) {
+//   console.log(a * b);
+// }
+// multiply(3, 3);
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+// console.log(multiply(3, 3));
+
+// const multiply = function (a, b) {
+//   const result = a * b;
+//   return result;
+// };
+// console.log(multiply(3, 3));
+
+// let value = 1000;
+
+// const add = (a, b) => {
+//   value = 1;
+//   return a + b;
+// };
+// console.log(value);
+
+// const scores = [1, 2, 3, 4, 5];
+// const deletedScores = scores.splice(2, 2, 6, 7);
+// console.log(scores);
+// console.log(deletedScores);
+
+// const scores = [1, 2, 3, 4, 5];
+// const deletedScores = scores.slice(3);
+// console.log(scores);
+// console.log(deletedScores);
+
+const names = ["mango", "poly", "ajax", "kiwi"];
+console.log(names[2]);
